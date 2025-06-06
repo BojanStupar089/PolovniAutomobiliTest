eb802075f6912009b791cc3a2b978d15b93d05d6
BojanStupar089 <bojanstupar089@gmail.com>
2025-06-06 21:29:27 +0200

    Added login part

--- polovniautomobili.py
+++ polovniautomobili.py
@@ -19,6 +19,32 @@ def driver():
     yield ch_driver
     ch_driver.quit()
 
+def login(driver):
+    wait = WebDriverWait(driver, 30)
+
+    menu = wait.until(
+        EC.presence_of_element_located((By.CLASS_NAME, "top-menu-profile"))
+    )
+
+    ActionChains(driver).move_to_element(menu).perform()
+
+
+    dropdown_link =wait.until(
+        EC.visibility_of_element_located(
+            (By.XPATH, '//ul[contains(@class, "my_profile_dropdown")]//a[text()="Prijavi se"]'))
+    )
+
+
+    dropdown_link.click()
+
+    wait.until(EC.visibility_of_element_located((By.ID,"username_header")))
+
+    driver.find_element(By.ID,"username_header").send_keys("bojanstupar1989@gmail.com")
+    driver.find_element(By.ID,"next-step").click()
+    wait.until(EC.visibility_of_element_located((By.ID, "password_header")))
+    driver.find_element(By.ID,"password_header").send_keys("Celarevo44!")
+    driver.find_element(By.NAME,"login").click()
+
 def test_open_chrome_and_go_to_polovni_automobili(driver):
 
    assert "Automobili " in driver.find_element(By.TAG_NAME,"h1").text,"Not good"
@@ -176,5 +202,117 @@ def test_register_tos_required(driver):
     is_invalid = driver.find_element(By.ID, "tos").get_attribute("validationMessage")
 
     assert "Please check this box if you want to proceed." in is_invalid, "Error"
+
+
+def test_login_succesfully(driver):
+   wait = WebDriverWait(driver, 30)
+   login(driver)
+
+   email_element = wait.until(
+        EC.visibility_of_element_located((By.XPATH, '//div[contains(text(),"MOJ PROFIL")]'))
+    )
+
+   email_text= email_element.text.strip()
+   assert "MOJ PROFIL" in email_text,"Error"
+
+
+def test_login_email_invalid(driver):
+   wait = WebDriverWait(driver, 30)
+   menu = wait.until(
+       EC.presence_of_element_located((By.CLASS_NAME, "top-menu-profile"))
+   )
+
+   ActionChains(driver).move_to_element(menu).perform()
+
+   dropdown_link = wait.until(
+       EC.visibility_of_element_located(
+           (By.XPATH, '//ul[contains(@class, "my_profile_dropdown")]//a[text()="Prijavi se"]'))
+   )
+
+   dropdown_link.click()
+
+   wait.until(EC.visibility_of_element_located((By.ID, "username_header")))
+
+   driver.find_element(By.ID, "username_header").send_keys("")
+   driver.find_element(By.ID, "next-step").click()
+
+   error_element = wait.until(
+       EC.visibility_of_element_located((By.CLASS_NAME, "log_error"))
+   )
+
+   # Extract and assert the text
+   error_text = error_element.text.strip()
+   assert "Ne postoji nalog sa ovom mail adresom." in error_text,"Error"
+
+
+def test_login_password_invalid(driver):
+    wait = WebDriverWait(driver, 30)
+    menu = wait.until(
+        EC.presence_of_element_located((By.CLASS_NAME, "top-menu-profile"))
+    )
+
+    ActionChains(driver).move_to_element(menu).perform()
+
+    dropdown_link = wait.until(
+        EC.visibility_of_element_located(
+            (By.XPATH, '//ul[contains(@class, "my_profile_dropdown")]//a[text()="Prijavi se"]'))
+    )
+
+    dropdown_link.click()
+
+    wait.until(EC.visibility_of_element_located((By.ID, "username_header")))
+
+    driver.find_element(By.ID, "username_header").send_keys("bojanstupar1989@gmail.com")
+    driver.find_element(By.ID, "next-step").click()
+
+    driver.find_element(By.ID,"password_header").send_keys("Cela")
+    driver.find_element(By.NAME, "login").click()
+
+    alert_element = wait.until(
+        EC.visibility_of_element_located((By.CLASS_NAME, "uk-alert-danger"))
+    )
+
+
+    alert_text = alert_element.text.strip()
+    assert "Proveri da li si dobro uneo e-mail i/ili šifru" in alert_text,"Error"
+
+def test_login_click_forgot_password_link(driver):
+    wait = WebDriverWait(driver, 50)
+    menu = wait.until(
+        EC.presence_of_element_located((By.CLASS_NAME, "top-menu-profile"))
+    )
+
+    ActionChains(driver).move_to_element(menu).perform()
+
+    dropdown_link = wait.until(
+        EC.visibility_of_element_located(
+            (By.XPATH, '//ul[contains(@class, "my_profile_dropdown")]//a[text()="Prijavi se"]'))
+    )
+
+    dropdown_link.click()
+
+    wait.until(EC.visibility_of_element_located((By.ID, "username_header")))
+
+    driver.find_element(By.ID, "username_header").send_keys("bojanstupar1989@gmail.com")
+    driver.find_element(By.ID, "next-step").click()
+
+    driver.find_element(By.ID, "password_header").send_keys("")
+
+    driver.find_element(By.ID,"forgot_password").click()
+
+    send_button = wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//button[text()='Pošalji']"))
+    )
+
+    # Click the button
+    send_button.click()
+
+
+
+
+
+
+
+
 
 
